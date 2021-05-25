85570e1f4cdc3168d6e509319e8848465179ca7c
Mohit4402 <panchalmohit51@gmail.com>
2021-05-25 14:18:41 +0530

    login page design code
    this is my first day code of intenship

---
+++ login_page.php
@@ -0,0 +1,31 @@
+
+
+<html>
+    <head>
+        <title>
+            Pages
+        </title>
+
+    </head>
+        <body id="body">
+            <header>
+
+            </header>
+            
+            <h1 align="center">LOGIN PAGE</h1>
+            <div id="Login">
+                <form align="center" method="POST">
+                    <h1>Login</h1>
+                    <input type="email" id="corner" name="email" placeholder=" email" required><br>
+                    <input type="password" id="corner" name="pass" placeholder="password" required><br>
+                    <input type="button" id="button"  value="LOGIN" name="login" ><br>
+                    <p>ForgatPassword ? </p><p>Register !!</p>
+                </form>
+                
+            </div>
+            
+        </body>
+</html>
+
+
+<link rel="stylesheet" type="text/css" href="d.css" media="screen"/>
